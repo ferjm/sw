83b08bce855e2edb9784e2a7a5934fff67790690
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2015-01-08 17:57:45 +0100

    Adding a img

--- app/service_worker_files.js
+++ app/service_worker_files.js
@@ -2,27 +2,30 @@
 
 var kCacheFiles = [
   // html
-  '/sw/app/index.html',
+  'index.html',
 
   // scripts
-  '/sw/app/js/utils.js',
-  '/sw/app/js/bootstrap.js',
-  '/sw/app/js/string-polyfill.js',
-  '/sw/app/js/cache-polyfill.js',
+  'js/utils.js',
+  'js/bootstrap.js',
+  'js/string-polyfill.js',
+  'js/cache-polyfill.js',
 
   // service worker helpers
-  '/sw/app/js/service/api.js',
-  '/sw/app/js/service/utils.js',
-  '/sw/app/js/service/worker_api.js',
+  'js/service/api.js',
+  'js/service/utils.js',
+  'js/service/worker_api.js',
 
   // protocols
-  '/sw/app/js/protocols/ipdl.js',
-  '/sw/app/js/protocols/ipdl_parser.js',
-  '/sw/app/js/protocols/bridge.js',
-  '/sw/app/js/protocols/store.js',
-  '/sw/app/js/protocols/message.js',
-  '/sw/app/js/protocols/utils/uuid.js',
-  '/sw/app/js/protocols/protocol_helper.js',
-  '/sw/app/js/protocols/ipdl/PUpdate.ipdl',
-  '/sw/app/js/protocols/ipdl/PService.ipdl'
+  'js/protocols/ipdl.js',
+  'js/protocols/ipdl_parser.js',
+  'js/protocols/bridge.js',
+  'js/protocols/store.js',
+  'js/protocols/message.js',
+  'js/protocols/utils/uuid.js',
+  'js/protocols/protocol_helper.js',
+  'js/protocols/ipdl/PUpdate.ipdl',
+  'js/protocols/ipdl/PService.ipdl',
+
+  // res
+  'sw.png'
 ];
